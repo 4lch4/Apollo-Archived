31ba171329e0cef7ae4bf4097cc2ecc021d22eae
Devin Leaman <dleaman212@gmail.com>
2017-08-21 21:02:23 -0500

    Added basic support for RAM/CPU/FSSize data
    Currenty the app will display the current CPU load, RAM in use, and the percentage used of each mounted drive.
    
    Also, if you execute the script as `node .\Apollo.js pollRate` where pollRate is the rate in ms you want it to update, it will update at that interval. Otherwise, it updates every 5 seconds.

--- Apollo.js
+++ Apollo.js
@@ -9,6 +9,72 @@ const LineBuffer = CLI.LineBuffer;
 
 const Gauge = CLI.Gauge;
 
+function getMemLine(outputBuffer) {
+    return new Promise((resolve, reject) => {
+        si.mem().then(data => {
+            let memTotal = data.total;
+            let memFree = data.free;
+            let memUsed = memTotal - memFree;
+            let memHuman = Math.ceil(memUsed / 1000000) + ' MB';
+
+            resolve(new Line(outputBuffer)
+                .column('Memory In Use', 20, [clc.cyan])
+                .column(Gauge(memUsed, memTotal, 20, memTotal * 0.8, memHuman), 30)
+                .fill().store());
+        });
+    });
+}
+
+function getCurrLoadLine(outputBuffer) {
+    return new Promise((resolve, reject) => {
+        si.currentLoad().then(data => {
+            let currLoad = data.currentload;
+            let cpuHuman = Math.ceil(currLoad).toFixed(0) + '%';
+
+            resolve(new Line(outputBuffer)
+                .column('Current CPU Load', 20, [clc.cyan])
+                .column(Gauge(currLoad, 100, 20, 80, cpuHuman), 30)
+                .fill().store());
+        });
+    });
+}
+
+function getFSSizeLine(outputBuffer) {
+    return new Promise((resolve, reject) => {
+        si.fsSize().then((data, err) => {
+            if (err) {
+                console.log('There was a problem...');
+                console.log(err);
+                return;
+            }
+
+            let lines = [];
+
+            for (let x = 0; x < data.length; x++) {
+                let fsHuman = Math.ceil(data[x].use).toFixed(0) + '%';
+                lines += new Line(outputBuffer)
+                    .column('Drive ' + data[x].fs.toString(), 20, [clc.cyan])
+                    .column(Gauge(data[x].use, 100, 20, 80, fsHuman), 30)
+                    .fill().store()
+            }
+
+            resolve(lines);
+        });
+    });
+}
+
+async function getLatestData(outputBuffer) {
+    const memLine = await getMemLine(outputBuffer);
+    const cpuLine = await getCurrLoadLine(outputBuffer);
+    const fsSizeLines = await getFSSizeLine(outputBuffer);
+}
+
+let pollRate = 5000;
+
+if (!isNaN(process.argv[2])) {
+    pollRate = process.argv[2];
+}
+
 AsyncPolling(end => {
     const outputBuffer = new LineBuffer({
         x: 0,
@@ -17,28 +83,9 @@ AsyncPolling(end => {
         height: 'console'
     });
 
-    si.mem().then(data => {
-        let memTotal = data.total;
-        let memFree = data.free;
-        let memUsed = memTotal - memFree;
-        let memHuman = Math.ceil(memUsed / 1000000) + ' MB';
-
-        new Line(outputBuffer)
-            .column('Memory In Use', 20, [clc.cyan])
-            .column(Gauge(memUsed, memTotal, 20, memTotal * 0.8, memHuman), 30)
-            .fill().store();
-
-        si.currentLoad().then(data => {
-            let currLoad = data.currentload;
-            let cpuHuman = Math.ceil(currLoad).toFixed(0) + '%';
-
-            new Line(outputBuffer)
-                .column('Current CPU Load', 20, [clc.cyan])
-                .column(Gauge(currLoad, 100, 20, 80, cpuHuman), 30)
-                .fill().store();
-
-            outputBuffer.output();
-            end();
-        });
+    getLatestData(outputBuffer).then(() => {
+        console.log('\x1Bc');
+        outputBuffer.output();
+        end();
     });
-}, 5000).run();
+}, pollRate).run();
